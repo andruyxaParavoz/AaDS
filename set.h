5604e127a89d94f3f00957e4630a680e8daa04c6
OrangeFruittbtw <rabcikovvlad7@gmail.com>
2025-10-12 00:01:32 +0400

    создал класс, прописал конструктор, -//- копирования, -//- для массива, деструктор, ф-ции на проверку равенства и наличия э-та

---
+++ set.h
@@ -0,0 +1,48 @@
+#ifndef SET_H
+#define SET_H
+
+#include <iostream>
+#include <stdexcept>
+
+template <typename T>
+class Set {
+private:
+	T* data_;
+	std::size_t size_;
+	static constexpr double EPSILON = 1e-6;
+
+	bool is_equal(const T& a, const T& b) const {
+		if constexpr (std::is_floating_point_v<T>) {
+			return std::abs(a - b) < EPSILON;
+		}
+		else {
+			return a == b;
+		}
+	}
+
+	bool contains_internal(const T& value) const {
+		for (std::size_t i = 0; i < size_; ++i) {
+			if (is_equal(data_[i]), value) { return true; }
+		}
+		return false;
+	}
+
+public:
+	Set() : data_(nullptr), size_(0) {}
+
+	Set(const T* arr, std::size_t n) : data_(nullptr), size_(0) {
+		for (std::size_t i = 0; i < n; ++i) { add(arr[i]); }
+	}
+
+	Set(const Set& other) : data_(nullptr), size_(0) {
+		if (other.size_ > 0) {
+			data_ = new T[other.size_];
+			for (std::size_t i = 0; i < other.size_; ++i) { data_[i] = other.data_[i]; }
+		}
+		size_ = other.size_;
+	}
+
+	~Set() { delete[] data_; }
+};
+
+#endif // !SET_H
